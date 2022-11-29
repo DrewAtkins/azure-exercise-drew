5d58dac616dae4afdfa851edc039364ff9780cea
Drew Atkins <85863536+DrewAtkins@users.noreply.github.com>
2022-11-29 11:34:53 -0500

    Add files via upload

---
+++ spring-boot-github-actions-ci-cd-feature-deploy_slot/src/main/java/com/example/demo/controller/GreetingsController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingsController {
+
+    @GetMapping("/")
+    public String getGreeting(){
+        return "Hello World. Built completely from pipeline.";
+    }
+}
